97b0735a49e015364328077f947000fd3b65d069
Hellieos <bodnar.olga2018@gmail.com>
2025-10-13 19:16:51 +0200

    corrections HW 7

--- qa_homework_seven_ts/src/arrow-functions.ts
+++ qa_homework_seven_ts/src/arrow-functions.ts
@@ -1,8 +1,7 @@
 // те саме завдання із стрілковою функцією (TypeScript)
-export const sumArrayArrow = (arr: readonly (number | string)[]): number => {
-// (опціонально) рантайм-перевірка, лишаємо як у JS-варіанті
-    if (!Array.isArray(arr)) {
-        console.warn('sumArrayArrow: аргумент має бути масивом');
+export const sumArrayArrow = (arr: (number | string)[]): number => {
+    if (arr.length === 0) {
+        console.warn('sumArray: масив порожній');
         return 0;
     }
 
@@ -15,7 +14,9 @@ export const sumArrayArrow = (arr: readonly (number | string)[]): number => {
 // масиви
 const numArr: number[] = [7, 3, 9];
 const strArr: string[] = ['4', '5', 'six', '7'];
+const emptyArr: string[] = [];
 
 // виклики
 console.log('sumArrayArrow(numArr) =', sumArrayArrow(numArr)); // 19
 console.log('sumArrayArrow(strArr) =', sumArrayArrow(strArr)); // 16
+console.log('sumArrayArrow(emptyArr) =', sumArrayArrow(emptyArr)); //масив порожній > 0
--- qa_homework_seven_ts/src/function.ts
+++ qa_homework_seven_ts/src/function.ts
@@ -1,8 +1,8 @@
 // звичайна функція, що приймає масив і повертає суму його елементів (TypeScript)
-// string значення перетворити в число > якщо не вийшло — ігнорувати
-export function sumArray(arr: readonly (number | string)[]): number {
-    if (!Array.isArray(arr)) {
-        console.warn('sumArray: аргумент має бути масивом');
+// string значення перетворити в число
+export function sumArray(arr: (number | string)[]): number {
+    if (arr.length === 0) {
+        console.warn('sumArray: масив порожній');
         return 0;
     }
 
@@ -15,7 +15,9 @@ export function sumArray(arr: readonly (number | string)[]): number {
 // масиви
 const numbers: number[] = [1, 2, 3, 4, 5];
 const stringNumbers: string[] = ['10', '20', '30', 'not-a-number'];
+const emptyArr: string[] = [];
 
 // виклики
 console.log('sumArray(numbers) =', sumArray(numbers));             // 15
 console.log('sumArray(stringNumbers) =', sumArray(stringNumbers)); // 60 (рядки-конвертуються)
+console.log('sumArray(emptyArr) =', sumArray(emptyArr)); //масив порожній > 0
