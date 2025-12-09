090cb1a48ca250e3e97990dd2d9077bbcd331e9f
Hellieos <147742805+Hellieos@users.noreply.github.com>
2025-12-10 00:09:03 +0100

    Update book-details-cart.spec.ts
    removed the comments

--- qa_homework_eighteen/tests/book-details-cart.spec.ts
+++ qa_homework_eighteen/tests/book-details-cart.spec.ts
@@ -8,7 +8,6 @@ test.describe('Book Details and Viewing a Book in the Cart', () => {
         await expect(bookStorePage.headingBooksList).toContainText('Books List');
     });
 
-    // Тест-кейс 1: Переглянути деталі книги і додати до корзини
     test('Переглянути деталі книги і додати до корзини', async ({ bookStorePage, bookDetailsPage }): Promise<void> => {
         await bookStorePage.getProductTitle(0);
         const expectedTitle =  await bookStorePage.getProductTitle(0);
@@ -17,7 +16,6 @@ test.describe('Book Details and Viewing a Book in the Cart', () => {
         await bookDetailsPage.addViewedBookToCart(expectedTitle);
     });
 
-    // Тест-кейс 2: Додати до корзини і перевірити чи правильна додана книга
     test('Перевірити додану до корзини книгу', async ({ bookStorePage, bookDetailsPage }): Promise<void> => {
         await bookStorePage.bookTitles.nth(0).click();
         await bookDetailsPage.getBookTitleFromH3();
