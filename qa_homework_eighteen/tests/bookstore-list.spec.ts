07cd04c9c46ae48e53bdce6d1c8fa381b0f2f4f4
Hellieos <147742805+Hellieos@users.noreply.github.com>
2025-12-10 00:09:47 +0100

    Update bookstore-list.spec.ts
    removed the comments

--- qa_homework_eighteen/tests/bookstore-list.spec.ts
+++ qa_homework_eighteen/tests/bookstore-list.spec.ts
@@ -8,14 +8,12 @@ test.describe('Bookstore List page', () => {
         await expect(bookStorePage.headingBooksList).toContainText('Books List');
     });
 
-    // Тест-кейс 1: Застосувати фільтр New і перевірити сортування
     test('Застосувати фільтр New', async ({ bookStorePage }): Promise<void> => {
         await expect(bookStorePage.sortNewButton).toBeVisible();
 
         await bookStorePage.sortNew();
     });
 
-    // Тест-кейс 2: Застосувати фільтр Descending і перевірити сортування
     test('Застосувати фільтр Descending', async ({ bookStorePage }): Promise<void> => {
         await expect(bookStorePage.priceDropdown).toBeVisible();
 
@@ -25,7 +23,6 @@ test.describe('Bookstore List page', () => {
         await expect(bookStorePage.productCard.nth(0)).toContainText('Agile Testing');
     });
 
-    // Тест-кейс 3: Застосувати free text пошук і перевірити сортування
     test('Застосувати free text пошук', async ({ bookStorePage }): Promise<void> => {
         await expect(bookStorePage.searchInput).toBeVisible();
 
@@ -38,7 +35,6 @@ test.describe('Bookstore List page', () => {
         expect(title.toLowerCase()).toContain('javascript');
     });
 
-    // Тест-кейс 4: Додати всі книги в корзину і перевірити badge
     test('Додати всі книги в корзину', async ({ bookStorePage }): Promise<void> => {
         const productCount = await bookStorePage.getProductCount();
         await bookStorePage.addAllBooksToCart();
