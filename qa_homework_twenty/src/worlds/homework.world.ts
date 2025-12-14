38991740aaa20232a09c1b546db2bd9a48d3dd47
Hellieos <bodnar.olga2018@gmail.com>
2025-12-14 11:21:55 +0100

    correction

--- qa_homework_twenty/src/worlds/homework.world.ts
+++ qa_homework_twenty/src/worlds/homework.world.ts
@@ -3,15 +3,13 @@ import type { Browser, BrowserContext, Page } from 'playwright';
 import { LoginPage } from '../pages/login-page';
 
 export class HomeworkWorld extends World {
-    // shared via hooks
+
     public browser!: Browser;
     public context!: BrowserContext;
     public page!: Page;
 
-    // page objects
     public loginPage!: LoginPage;
 
-    // test data / scenario state
     public readonly validUsername: string;
     public readonly validPassword: string;
     public readonly invalidUsername = 'wrong-user';
@@ -23,8 +21,6 @@ export class HomeworkWorld extends World {
         this.validPassword = process.env.LOGIN_PASSWORD ?? '';
 
         if (!this.validUsername || !this.validPassword) {
-        // щоб падало одразу зрозуміло
-        // (можеш прибрати, якщо викладач не любить throw у World)
             throw new Error('Missing env vars: LOGIN_USERNAME / LOGIN_PASSWORD');
         }
     }
